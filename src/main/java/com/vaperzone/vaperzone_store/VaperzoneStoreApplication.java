530499099a9ffa107d38f68f3e62c2149a5c5cea
Asper1122 <sergiolo866@gmail.com>
2025-09-02 20:26:18 +0200

    first commit

---
+++ src/main/java/com/vaperzone/vaperzone_store/VaperzoneStoreApplication.java
@@ -0,0 +1,13 @@
+package com.vaperzone.vaperzone_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaperzoneStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaperzoneStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vaperzone/vaperzone_store/VaperzoneStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaperzone.vaperzone_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaperzoneStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
